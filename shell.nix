9beb154b2691cf28eba3ae50b346f28df41eb13b
Mathieu GRZYBEK <mathieu.grzybek@s3ns.io>
2024-07-30 15:54:48 +0200

    Adding pre-commit modules to the dev shell

--- shell.nix
+++ shell.nix
@@ -12,6 +12,16 @@
       jq
       kubectl
       libvirt
+
+      ## Problems using pip install on `pre-commit run --all`
+      #pre-commit
+      #python312Packages.pre-commit-hooks
+      #python312Packages.pytest
+      #python312Packages.mypy
+      #python312Packages.twine
+      #python312Packages.types-requests
+      #python312Packages.types-setuptools
+
       talosctl
       wget
       which
